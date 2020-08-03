459ba131bb3189af486b3dacb12be2f7b3d082cf
pranjal <pranjalmisra2410@gmail.com>
2020-08-03 13:21:06 +0530

    Added hamburger menu logic

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const main = document.querySelector('body');
 function stickyNav() {
 	if (window.scrollY >= topToNav) {
 		main.style.paddingTop = nav.offsetHeight + 'px';
-		console.log(window.scrollY, topToNav, nav.offsetHeight);
 		main.classList.add('fixed');
 	} else {
 		main.style.paddingTop = 0;
@@ -14,3 +13,15 @@ function stickyNav() {
 }
 
 window.addEventListener('scroll', stickyNav);
+const hamIcon = document.querySelector('#ham-icon');
+let flag;
+let list = document.querySelector('#list');
+hamIcon.addEventListener('click', () => {
+	if (flag) {
+		flag = false;
+		list.classList.add('u-hidden');
+	} else {
+		flag = true;
+		list.classList.remove('u-hidden');
+	}
+});
