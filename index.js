2db6a603c37b03ad5586d8e0c044b0d3ee82cc8f
pranjal <pranjalmisra2410@gmail.com>
2020-09-03 15:37:57 +0530

    Added GSAP animation

--- index.js
+++ index.js
@@ -25,3 +25,12 @@ hamIcon.addEventListener('click', () => {
 		list.classList.remove('u-hidden');
 	}
 });
+
+const words = [ 'Web Developer', 'Full Stack Developer', 'Software Engineer' ];
+let masterTL = gsap.timeline({ repeat: -1 });
+
+words.forEach((word) => {
+	let tl = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 1.5 });
+	tl.to('.heading__primary--sub', { duration: 1.5, text: word });
+	masterTL.add(tl);
+});
